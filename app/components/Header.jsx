6fe5ac0619e8a7e1a7aaa98e5bc3774f89df887a
Solène <81546747+solenedel@users.noreply.github.com>
2024-01-06 20:47:18 +0100

    various styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,7 +1,19 @@
+import { Inter, DM_Serif_Display, Golos_Text } from 'next/font/google';
+
+const dmSerifDisplay = DM_Serif_Display({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
 function Header() {
   return (
-    <header className="flex px-10 py-5">
-      <h1 className="text-blue-950 text-3xl ">Luxe4ever</h1>
+    <header className="flex px-10 py-8 flex justify-center bg-blue-950">
+      <h1
+        className={dmSerifDisplay.className}
+        style={{ fontSize: '40px', color: '#f1e4ff', letterSpacing: '3px' }}>
+        {' '}
+        Forever.Luxe
+      </h1>
     </header>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,13 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between  text-sm lg:flex">
-        This is the Home Page
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h2 className="text-2xl self-center">
+        Your NFT marketplace for second-hand luxury.
+      </h2>
+      <div className="flex gap-x-32 mt-24 text-4xl font-semibold ">
+        <button className="bg-blue-950 rounded-sm p-3 text-white">Buy</button>
+        <button className="bg-blue-950 rounded-sm p-3 text-white">Sell</button>
       </div>
     </main>
   );
