6ad61f2fe82a38deb0e6b6af0cd11e37e0fae765
Solène <81546747+solenedel@users.noreply.github.com>
2024-01-11 12:02:00 +0100

    work on collections

--- app/collections/[collection]/page.jsx
+++ app/collections/[collection]/page.jsx
@@ -1,11 +1,13 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
+import { collectionsList } from '../../../data/collections';
 
 export default function CollectionPage({ params }) {
   const router = useRouter();
   const pathname = usePathname();
   const currentCollection = pathname.split('/').pop();
+  const collectionsObject = collectionsList;
 
   const [collection, setCollection] = useState(params.collection); // should this be intsantly set to params.collection?
 
@@ -15,12 +17,22 @@ export default function CollectionPage({ params }) {
 
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <h1 className="text-2xl text-center font-semibold">
+      <h1 className="text-2xl text-center font-semibold mb-20">
         Collection: {collection}
       </h1>
-      <section>👜 👑 👛 💼 💍</section>
+      {/* NFT carousel */}
+      <ul className="flex gap-x-16">
+        {collectionsObject[collection].map((NFT) => (
+          <li key={NFT.id} className="flex flex-col">
+            <span className="text-8xl">{NFT.photo}</span>
+            <span className="text-lg">{NFT.name}</span>
+            <span className="font-semibold text-xl">{NFT.price} ETH</span>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
 
-// export default client(CollectionPage);
+// TODO:
+// separate carousels for 'most viewed', 'most owners', etc
