a9627d0db46ba30e4a2d4f072302569d18be98e5
Solène <81546747+solenedel@users.noreply.github.com>
2024-01-15 12:17:22 +0100

    debugging: trying to map thru new collections structure

--- app/collections/[collection]/[nftId]/page.jsx
+++ app/collections/[collection]/[nftId]/page.jsx
@@ -6,11 +6,12 @@ import { collectionsList } from '../../../../data/collections';
 export default function NFTPage(params) {
   const [nftId, setNftId] = useState(params.params.nftId);
   const [collection, setcollection] = useState(params.params.collection);
+  const collectionsObject = collectionsList;
 
   return (
     <main className="flex min-h-screen flex-col p-24">
-      {console.log('PARAMS ==========')}
       Page for NFT: {nftId}, from collection: {collection}
+      {/* {console.log(collectionsObject[collection][id])} */}
     </main>
   );
 }
--- app/collections/[collection]/page.jsx
+++ app/collections/[collection]/page.jsx
@@ -11,10 +11,6 @@ export default function CollectionPage({ params }) {
 
   const [collection, setCollection] = useState(params.collection); // should this be intsantly set to params.collection?
 
-  useEffect(() => {
-    console.log('========== PATHNAME!!!!!', typeof pathname);
-  }, [pathname]);
-
   const handleClick = (id) => {
     router.push(`${pathname + '/' + id}`);
   };
@@ -26,14 +22,15 @@ export default function CollectionPage({ params }) {
       </h1>
       {/* NFT carousel */}
       <ul className="flex w-full gap-x-24  justify-center">
-        {collectionsObject[collection].map((NFT) => (
+        {collectionsObject[collection].map((NFT, i) => (
           <li
-            onClick={() => handleClick(NFT.id)}
-            key={NFT.id}
+            onClick={() => handleClick(NFT[i])}
+            key={NFT[i][id]}
             className="hover:scale-105 hover:cursor-pointer w-2/3 items-center flex flex-col gap-y-2 shadow-xl border-2 border-gray-900 hover:bg-gray-900 hover:text-pink-50 rounded-md p-5">
-            <span className="text-8xl">{NFT.photo}</span>
-            <span className="text-lg">{NFT.name}</span>
-            <span className="font-semibold text-xl">{NFT.price} ETH</span>
+            {console.log('NFT =========', NFT[i])}
+            {/* <span className="text-8xl">{NFT.photo}</span> */}
+            {/* <span className="text-lg">{NFT[name]}</span>
+            <span className="font-semibold text-xl">{NFT[price]} ETH</span> */}
           </li>
         ))}
       </ul>
