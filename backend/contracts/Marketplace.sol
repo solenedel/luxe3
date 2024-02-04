0f3ae7bd5ebd528cc62ce0df7be30e96a1292b5b
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-04 21:43:07 +0100

    work on smart contracts

--- backend/contracts/Marketplace.sol
+++ backend/contracts/Marketplace.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 import "./NFTCollection.sol";
 
 /// @title this contract handles marketplace actions, such as: creating a new collection (one per user), marking items as received, and more.
@@ -13,15 +12,35 @@ import "./NFTCollection.sol";
 /// @notice 
 
 contract Marketplace is Ownable {
+
+    // ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️ VARIABLES ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️
+    struct User {
+      bool hasCollection; 
+    }
     
+    mapping (address => User) users;
+    
+    
+
+    
+    // ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️ CONSTRUCTOR ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️
+    
+    /// @notice Owner corresponds to the admin of the marketplace. 
     constructor() Ownable(msg.sender) {}
+
+
+    // ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️ FUNCTION: DEPLOY NEW COLLECTION ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️
 
     /// @notice This function deploys a new NFTCollection contract. Only one collection is allowed per user.
     /// @param _name is the name of the new NFT collection, ex: "My Collection" and _symbol is the symbol, ex: "MC"
-    /// @return 
-        function deployNewNFTCollection(string memory _name, string memory _symbol) public {
-        // todo - check if user already has a collection or not.
-        NFTCollection newCollection = new NFTCollection(_name, _symbol);
+
+    function deployNewNFTCollection(string memory _name, string memory _symbol) public {   
+       
+      require(users[msg.sender].hasCollection == false, "You have already created an NFT collection.");
+
+      NFTCollection newCollection = new NFTCollection(_name, _symbol);
+      
+      users[msg.sender].hasCollection = true; //todo - reentrancy here?
         
     }
 }
--- backend/contracts/NFTCollection.sol
+++ backend/contracts/NFTCollection.sol
@@ -3,14 +3,32 @@
 // This contract handles each new collection of NFTs
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTCollection is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+
+/// @title this contract handles a collection of NFTs. Each user can create one collection.
+/// @author Solene D.
+/// @notice 
+contract NFTCollection is ERC721URIStorage, Ownable {
+
+// ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️ VARIABLES ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️
+    uint256 private _tokenIdCounter;
+
+// ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️ CONSTRUCTOR ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️
     constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol) {}
+        ERC721(_name, _symbol) Ownable(msg.sender) {}
 
- 
+ // ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️ FUNCTION: SAFE MINT ◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️
+
+ function safeMint(address _to, string memory _URI) public onlyOwner {
+        _tokenIdCounter++;
+        _safeMint(_to, _tokenIdCounter);
+        _setTokenURI(_tokenIdCounter, _URI);
+
+    }
+
+
 }
