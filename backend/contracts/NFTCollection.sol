ce118046b11d7e6483dd66df189ac2d2985ea8c6
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-04 20:54:52 +0100

    begin smart contracts

---
+++ backend/contracts/NFTCollection.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+
+contract NFTCollection is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol) {}
+
+ 
+}
