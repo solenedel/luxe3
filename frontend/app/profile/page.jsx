dc20c037d6cc3e4da4a4f583a5baeded861e679f
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-06 16:41:48 +0100

    work on profile page

--- frontend/app/profile/page.jsx
+++ frontend/app/profile/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Modal from '../components/Modal';
+import Modal from '../components/modals/NewCollectionModal';
 
 function ProfilePage() {
   const [showModal, setShowModal] = useState(false);
@@ -22,13 +22,43 @@ function ProfilePage() {
           ''
         )}
       </section>
-      {/* <section>
+      <section className="mt-12 text-xl">
+        <p>
+          You have created your collection.{' '}
+          <button
+            type="submit"
+            className="text-xl text-gray-900 hover:underline ml-20 hover:text-violet-500">
+            ✏ Edit collection
+          </button>
+        </p>
+        <span className="flex gap-x-10">
+          <button
+            type="submit"
+            className="mt-6 text-xl h-fit font-semibold bg-gradient-to-br from-orange-400 to-violet-400 rounded-sm p-2 shadow-lg text-gray-900 hover:translate-y-1">
+            Add NFT
+          </button>
+          <button
+            type="submit"
+            className="mt-6 text-xl h-fit font-semibold bg-gradient-to-br from-orange-400 to-violet-400 rounded-sm p-2 shadow-lg text-gray-900 hover:translate-y-1">
+            Edit NFTs
+          </button>
+        </span>
+      </section>
+    </main>
+  );
+}
+
+export default ProfilePage;
+{
+  /* <section>
         <h2>Your collection</h2>
         <button className="text-xl font-semibold mt-8 bg-gradient-to-br from-orange-400 to-violet-400 rounded-sm p-4 shadow-lg text-gray-900 hover:translate-y-1">
           Add item
         </button>
-      </section> */}
-      {/* <aside className="bg-sky-200 border-2 border-sky-300 p-3 w-2/3 rounded-md">
+      </section> */
+}
+{
+  /* <aside className="bg-sky-200 border-2 border-sky-300 p-3 w-2/3 rounded-md">
         <strong className="mr-2"> Pending:</strong>
         You have purchased ####. If you have received the physical item, please
         confirm reception so that the previous owner can receive their payment.
@@ -43,9 +73,5 @@ function ProfilePage() {
       <section className="mt-14 text-left">
         <h2 className="text-xl font-semibold mb-5">♥ My favourites</h2>
         <p>You have no favourites yet. </p>
-      </section> */}
-    </main>
-  );
+      </section> */
 }
-
-export default ProfilePage;
