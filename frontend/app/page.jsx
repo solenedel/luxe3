ecf14465c58ed0b05b1c32d13002c9a129760c3e
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-07 12:32:33 +0100

    styling

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -3,25 +3,30 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex flex-col pt-16 items-center min-h-screen px-12 ">
-      <div className="bg-white w-fit h-fit border-4 border-gray-950 rounded-sm">
-        <h2 className="text-2xl tracking-wider py-2 text-center text-light italic text-rose-300 bg-gray-900">
+      <div className="bg-white w-fit h-fit border-4 border-gray-950 rounded-sm ">
+        <h2 className="text-2xl tracking-wider py-2 text-center text-light italic text-rose-200 bg-gray-900">
           Your NFT marketplace for second-hand luxury.
         </h2>
         <Image
           src="/images/Hero_img.png"
           width="700"
           height="300"
-          className="grayscale opacity-50"
+          className="grayscale opacity-50 hover:blur-sm"
         />
       </div>
-
-      <div className="relative flex gap-x-16 z-50 bottom-80">
-        <button className="text-3xl shadow-lg border-emerald-900 font-semibold mt-20 bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-4 shadow-lg text-gray-950 hover:translate-y-1">
+      <div className="relative flex gap-x-24 z-50 bottom-64 text-2xl ">
+        <button className="border-2 rounded-sm px-4 py-2 border-gray-900 bg-rose-200 hover:translate-y-1">
+          Sell
+        </button>
+        <button className="border-2 rounded-sm px-4 py-2 border-gray-900 bg-rose-200 hover:translate-y-1">
+          Browse
+        </button>
+        {/* <button className="text-3xl shadow-lg border-emerald-900 font-semibold mt-20 bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-4 shadow-lg text-gray-950 hover:translate-y-1">
           Sell
         </button>
         <button className="text-3xl shadow-lg border-emerald-900 font-semibold mt-20 bg-gradient-to-bl from-emerald-800 to-emerald-500 rounded-lg p-4 shadow-lg text-gray-950 hover:translate-y-1">
           Browse
-        </button>
+        </button> */}
       </div>
 
       <div className="w-fit flex items-center justify-center relative mt-10">
