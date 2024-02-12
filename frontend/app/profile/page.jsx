2fa5caf7a64d9b437a8a349abdaaac3f18026ce0
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-12 23:50:34 +0100

    add more conditional rendering

--- frontend/app/profile/page.jsx
+++ frontend/app/profile/page.jsx
@@ -27,66 +27,74 @@ function ProfilePage() {
     // console.log('GET COLLECTION🔥🔥🔥🔥🔥🔥🔥', data);
   };
 
-  return (
-    <main className="flex flex-col items-center pt-10  min-h-screen">
-      <h1 className="text-3xl w-full text-center tracking-wider py-3 text-pink-100 bg-gray-900/[0.7] font-semibold mb-10">
-        Your dashboard
-      </h1>
-      {userInfo.hasCollection == false ? (
-        <section className="flex items-baseline gap-x-10">
-          <aside className="text-xl backdrop-blur-sm">
-            You have not created your NFT collection yet.{' '}
-          </aside>
-          <button
-            onClick={() => setShowModal(!showModal)}
-            className="text-2xl shadow-lg border-emerald-900 font-semibold bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-2 shadow-lg text-gray-950 hover:translate-y-1">
-            Start collection
-          </button>
-
-          {showModal ? (
-            <NewCollectionModal
-              showModal={showModal}
-              setShowModal={setShowModal}
-            />
-          ) : (
-            ''
-          )}
-        </section>
-      ) : (
-        <section className="mt-12 text-xl">
-          {showModalB ? (
-            <AddNFTModal
-              showModalB={showModalB}
-              setShowModalB={setShowModalB}
-            />
-          ) : (
-            ''
-          )}
-
-          <div>
-            <h2 className="text-xl font-semibold">
-              Your collection: {collectionInfo.name} ({collectionInfo.symbol}){' '}
-            </h2>
-            <p>Collection address: {collectionInfo.contractAddress}</p>
-          </div>
-
-          <span className="flex gap-x-10">
+  if (isConnected) {
+    return (
+      <main className="flex flex-col items-center pt-10  min-h-screen">
+        <h1 className="text-3xl w-full text-center tracking-wider py-3 text-pink-100 bg-gray-900/[0.7] font-semibold mb-10">
+          Your dashboard
+        </h1>
+        {userInfo.hasCollection == false ? (
+          <section className="flex items-baseline gap-x-10">
+            <aside className="text-xl backdrop-blur-sm">
+              You have not created your NFT collection yet.{' '}
+            </aside>
             <button
-              onClick={() => setShowModalB(!showModalB)}
-              type="submit"
-              className="text-xl shadow-lg border-emerald-900 font-semibold mt-10 bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-2 shadow-lg text-gray-950 hover:translate-y-1">
-              Add NFT
+              onClick={() => setShowModal(!showModal)}
+              className="text-2xl shadow-lg border-emerald-900 font-semibold bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-2 shadow-lg text-gray-950 hover:translate-y-1">
+              Start collection
             </button>
-            <button
-              type="submit"
-              className="text-xl shadow-lg border-emerald-900 font-semibold mt-10 bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-2 shadow-lg text-gray-950 hover:translate-y-1">
-              Edit NFTs
-            </button>
-          </span>
-        </section>
-      )}
-    </main>
-  );
+
+            {showModal ? (
+              <NewCollectionModal
+                showModal={showModal}
+                setShowModal={setShowModal}
+              />
+            ) : (
+              ''
+            )}
+          </section>
+        ) : (
+          <section className="mt-12 text-xl">
+            {showModalB ? (
+              <AddNFTModal
+                showModalB={showModalB}
+                setShowModalB={setShowModalB}
+              />
+            ) : (
+              ''
+            )}
+
+            <div>
+              <h2 className="text-xl font-semibold">
+                Your collection: {collectionInfo.name} ({collectionInfo.symbol}){' '}
+              </h2>
+              <p>Collection address: {collectionInfo.contractAddress}</p>
+            </div>
+
+            <span className="flex gap-x-10">
+              <button
+                onClick={() => setShowModalB(!showModalB)}
+                type="submit"
+                className="text-xl shadow-lg border-emerald-900 font-semibold mt-10 bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-2 shadow-lg text-gray-950 hover:translate-y-1">
+                Add NFT
+              </button>
+              <button
+                type="submit"
+                className="text-xl shadow-lg border-emerald-900 font-semibold mt-10 bg-gradient-to-br from-emerald-800 to-emerald-500 rounded-lg p-2 shadow-lg text-gray-950 hover:translate-y-1">
+                Edit NFTs
+              </button>
+            </span>
+          </section>
+        )}
+      </main>
+    );
+  } else {
+    return (
+      <p className="flex flex-col text-xl items-center pt-10 min-h-screen">
+        You must log in to view your dashboard.
+      </p>
+    );
+  }
 }
 
 export default ProfilePage;
