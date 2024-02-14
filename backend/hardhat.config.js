dde49e477d847729bda3a763212e828df76cb139
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-14 23:53:07 +0100

    configure hardhat to allow unlimited contract size

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -4,4 +4,9 @@ require('solidity-coverage');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.20',
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true, // Allow contracts larger than  24KB
+    },
+  },
 };
