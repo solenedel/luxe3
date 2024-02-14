103e500f9e5361e523f84b552a1ae0a6f7307066
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-14 19:17:18 +0100

    add call to smart contract from deploy script

--- backend/scripts/deployMarketplace.js
+++ backend/scripts/deployMarketplace.js
@@ -1,12 +1,33 @@
 const hre = require('hardhat');
 
 async function main() {
-  // TODO add a signer
   const Marketplace = await hre.ethers.deployContract('Marketplace');
-
   await Marketplace.waitForDeployment();
 
   console.log(`Marketplace contract was deployed to: ${Marketplace.target}`);
+
+  const [admin] = await hre.ethers.getSigners();
+  const adminAddr = admin.address;
+
+  // deploy Admin's collection
+  const collectionName = 'Admin Collection';
+  const collectionSymbol = 'ADMIN';
+  const tx = await Marketplace.deployNewNFTCollection(
+    collectionName,
+    collectionSymbol
+  );
+
+  //const receipt = await tx.wait(); // Wait for transaction to be mined
+  // console.log('event', receipt.logs);
+
+  // Print events emitted by the transaction
+  // for (const event of receipt.logs || []) {
+  //   console.log(`Event: ${event.log}, Args: ${JSON.stringify(event.args)}`);
+  // }
+
+  console.log(
+    `New NFT collection "${collectionName}" (${collectionSymbol}) was deployed by .`
+  );
 }
 
 main().catch((error) => {
