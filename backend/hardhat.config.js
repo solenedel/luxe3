390250f9069c22c83423beea1b97d56c5bcd0d61
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-15 10:56:02 +0100

    edit hardhat config & deploy to sepolia

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -10,18 +10,25 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.8.20',
+        version: '0.8.20', // should match both contracts?
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200, // Adjust the number of runs for the optimizer
+          },
+        },
       },
     ],
   },
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true, // ok because not deployed on mainnet
-      sepolia: {
-        url: ALCHEMY_SEPOLIA_RPC_URL,
-        accounts: [`0x${PRIVATE_KEY}`], //private key of addr that deployed the contract
-        chainId: 11155111,
-      },
+    },
+    sepolia: {
+      // allowUnlimitedContractSize: true,
+      url: ALCHEMY_SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`], //private key of addr that deployed the contract
+      chainId: 11155111,
     },
   },
 };
