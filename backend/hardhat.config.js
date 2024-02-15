ba36e0cd1254ca5121be9e37e0f71c99781f9ae3
Solène <81546747+solenedel@users.noreply.github.com>
2024-02-15 10:28:14 +0100

    start setting up hardhat config for deployment

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,12 +1,27 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('solidity-coverage');
+require('dotenv/config');
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const ALCHEMY_SEPOLIA_RPC_URL = process.env.ALCHEMY_SEPOLIA_RPC_URL || '';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.20',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.20',
+      },
+    ],
+  },
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: true, // Allow contracts larger than  24KB
+      allowUnlimitedContractSize: true, // ok because not deployed on mainnet
+      sepolia: {
+        url: ALCHEMY_SEPOLIA_RPC_URL,
+        accounts: [`0x${PRIVATE_KEY}`], //private key of addr that deployed the contract
+        chainId: 11155111,
+      },
     },
   },
 };
